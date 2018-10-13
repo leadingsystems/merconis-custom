9d61ac5a1e0e6d11ff6e8f4a2d6419f64ace7827
vstuber <stuber@leadingsystems.de>
2018-10-13 11:17:07 +0200

    Fix wrong array key in global language array

--- src/Resources/contao/languages/en/tl_merconis_custom_dummy.php
+++ src/Resources/contao/languages/en/tl_merconis_custom_dummy.php
@@ -14,8 +14,8 @@ $GLOBALS['TL_LANG']['tl_merconis_custom_dummy']['description_legend'] = 'Descrip
 /*
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_ls_api_user']['new'] = array('New record', 'Add a new record');
-$GLOBALS['TL_LANG']['tl_ls_api_user']['edit'] = array('Edit record', 'Edit record with ID %s');
-$GLOBALS['TL_LANG']['tl_ls_api_user']['delete'] = array('Delete record', 'Delete record with ID %s');
-$GLOBALS['TL_LANG']['tl_ls_api_user']['copy'] = array('Copy record', 'Copy record with ID %s');
-$GLOBALS['TL_LANG']['tl_ls_api_user']['show'] = array('Show details', 'Show details of record with ID %s');
+$GLOBALS['TL_LANG']['tl_merconis_custom_dummy']['new'] = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_merconis_custom_dummy']['edit'] = array('Edit record', 'Edit record with ID %s');
+$GLOBALS['TL_LANG']['tl_merconis_custom_dummy']['delete'] = array('Delete record', 'Delete record with ID %s');
+$GLOBALS['TL_LANG']['tl_merconis_custom_dummy']['copy'] = array('Copy record', 'Copy record with ID %s');
+$GLOBALS['TL_LANG']['tl_merconis_custom_dummy']['show'] = array('Show details', 'Show details of record with ID %s');
